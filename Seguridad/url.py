02b96a8ebc9b2d1befed8af1b39353e835797704
aavilesv <aavilesv@unemi.edu.ec>
2022-06-08 02:16:39 -0500

    Devolucion

--- Seguridad/url.py
+++ Seguridad/url.py
@@ -3,11 +3,12 @@ from django.urls import path
 from Seguridad.views import  login_session,logout_user
 from Seguridad.viewrecuperar import recuperar
 from Seguridad.perfil import perfil
+from Seguridad.view_registroperfil import  registro
 urlpatterns = [
 path('login/' ,login_session ,name='login'),
 path('logout/', logout_user ,name='logout'),
 path('recuperar/', recuperar ,name='recuperar'),
 path('perfil/', perfil ,name='perfil'),
-path('perfil/', perfil ,name='perfil'),
+path('registro/', registro ,name='registro'),
 
 ]
