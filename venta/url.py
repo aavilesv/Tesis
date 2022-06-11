dc4d9b5d6c0001419ec9b13daaa6a31584742032
aavilesv <aavilesv@unemi.edu.ec>
2022-06-11 13:00:42 -0500

    Devolucion

--- venta/url.py
+++ venta/url.py
@@ -4,10 +4,12 @@ from venta.view_cliente import cliente
 from venta.view_articulo import articulo
 from venta.view_marca import marca
 from venta.viewtipocategoria import  tipocategoria
+from venta.view_ubicacion import  ubicacion
 urlpatterns = [
 path('venta/' ,venta ,name='venta'),
 path('cliente/' ,cliente ,name='cliente'),
     path('articulo/' ,articulo ,name='articulo'),
+path('ubicacion/' ,ubicacion ,name='ubicacion'),
 path('tipocategoria/' ,tipocategoria ,name='tipocategoria'),
 path('marca/' ,marca ,name='marca'),
 
--- venta/view_ubicacion.py
+++ venta/view_ubicacion.py
@@ -6,15 +6,15 @@ from django.http import HttpResponse
 from Tesis.funciones import addUserData, render_to_pdf
 from django.db import transaction
 from django.shortcuts import render, redirect
-from venta.models import M_Marca, M_TipoCategoria
+from venta.models import M_Marca, M_TipoCategoria,M_Ubicacion
 
 
 @login_required(login_url='/seguridad/login/')
-def tipocategoria(request):
+def ubicacion(request):
     data ={
-        'titulo':'Consulta de Tipo de Categoría',
-        'model': 'Marca',
-        'ruta':'/venta/tipocategoria/',
+        'titulo':'Consulta de Ubicación',
+        'model': 'Ubicación',
+        'ruta':'/venta/ubicacion/',
         'user': request.user.username,
     }
     addUserData(request, data)
@@ -24,7 +24,7 @@ def tipocategoria(request):
             try:
                 with transaction.atomic():
                     if action == 'add':
-                        marca = M_TipoCategoria()
+                        marca = M_Ubicacion()
                         marca.descripcion = request.POST['nombre']
                         staa = True
                         if not 'status' in request.POST:
@@ -32,7 +32,7 @@ def tipocategoria(request):
                         marca.status = staa
                         marca.save()
                     if action == 'edit':
-                        marca = M_TipoCategoria.objects.select_related().get(pk=request.POST['id'])
+                        marca = M_Ubicacion.objects.select_related().get(pk=request.POST['id'])
                         marca.descripcion= request.POST['nombre']
                         staa = True
                         if not 'status' in request.POST:
@@ -42,7 +42,7 @@ def tipocategoria(request):
 
             except Exception as ex:
                 messages.error(request, 'Error, dato ya registrado')
-            return redirect('/venta/tipocategoria/')
+            return redirect('/venta/ubicacion/')
     else:
         # Por primera vez viaja por Get
         if 'action' in request.GET:
@@ -50,23 +50,23 @@ def tipocategoria(request):
             data['action'] = action
             if action == 'edit':
                 data['id'] = request.GET['id']
-                data['marca']=M_TipoCategoria.objects.get(pk=int(request.GET['id']))
-                return render(request, 'inventario/fromcategoria.html', data)
+                data['marca']=M_Ubicacion.objects.get(pk=int(request.GET['id']))
+                return render(request, 'inventario/ubacion_from.html', data)
             if action == 'add':
                 data['id'] = request.GET['id']
 
-                return render(request, 'inventario/fromcategoria.html', data)
+                return render(request, 'inventario/ubacion_from.html', data)
             if action == 'elim':
                 id = request.GET['id']
-                marca = M_TipoCategoria.objects.get(pk=int(id))
+                marca = M_Ubicacion.objects.get(pk=int(id))
                 marca.elim = False
                 marca.save()
-                return redirect('/venta/tipocategoria/')
+                return redirect('/venta/ubicacion/')
         elif 'imprime' in request.GET:
 
             marca = {
 
-                'marca': M_TipoCategoria.objects.filter(status=True),
+                'marca': M_Ubicacion.objects.filter(status=True),
                 'model': 'Marca'
             }
             pdf = render_to_pdf('inventario/pdfmarca.html', marca)
@@ -75,5 +75,5 @@ def tipocategoria(request):
 
         else:
             # Viaja por get
-            data['marca']=M_TipoCategoria.objects.all()
-            return render(request, 'inventario/tipocategoria.html', data)
+            data['marca']=M_Ubicacion.objects.all()
+            return render(request, 'inventario/ubicación.html', data)
