d289729ea001b6bef74ea72c204b9ea21f027156
aavilesv <aavilesv@unemi.edu.ec>
2022-06-11 17:36:34 -0500

    Devolucion

--- delivery/url.py
+++ delivery/url.py
@@ -2,9 +2,10 @@ from django.urls import path
 from venta.view_venta import venta
 from venta.view_cliente import cliente
 from delivery.views import productos
-from venta.view_articulo import articulo
+from delivery.viewcatalogo import catalogo
 urlpatterns = [
     path('contactos/' ,venta ,name='contactos'),
     path('cliente/' ,cliente ,name='cliente'),
     path('pedido/' ,productos ,name='productos'),
+    path('catalogo/',catalogo ,name='catalogo'),
 ]
