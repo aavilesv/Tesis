1619b963dc655ad221d4035cab82f1eb28de0a16
aavilesv <aavilesv@unemi.edu.ec>
2022-06-16 21:15:44 -0500

    Casi ultimo

--- compra/url.py
+++ compra/url.py
@@ -1,7 +1,10 @@
 from django.urls import path
 from compra.views_compra import compra
 from compra.view_proveedor import proveedor
+from compra.view_reportestock import reportearticulo
+
 urlpatterns = [
     path('compra/' ,compra ,name='compra'),
     path('proveedores/' ,proveedor ,name='proveedor'),
+    path('reportearticulo/',reportearticulo,name='reportearticulo'),
 ]
